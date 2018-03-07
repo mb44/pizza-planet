faac7a3f671f8d3c64a28105e3f00f581d0f3258
Morten Beuchert <mortenbeuchert@gmail.com>
2018-03-07 21:49:26 +0100

    Split the store into separate files

--- src/store/store.js
+++ src/store/store.js
@@ -1,66 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import menu from './modules/menu'
+import orders from './modules/orders'
+import users from './modules/users'
+
+//import * as actions from './actions.js'
+//import * as mutations from './mutations.js'
+//import * as getters from './getters.js'
+
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-    state: {
-        menuItems: {
-            1: {
-                'name': 'Margherita',
-                'description': 'A delicious tomato based pizza topped with mozzarella',
-                'options': [{
-                    'size': 9,
-                    'price': 6.95
-                }, {
-                    'size': 12,
-                    'price': 10.95
-                }]
-            },
-            2: {
-                'name': 'Pepperoni',
-                'description': 'A delicious tomato based pizza topped with mozzarella and pepperoni',
-                'options': [{
-                    'size': 9,
-                    'price': 7.95
-                }, {
-                    'size': 12,
-                    'price': 12.95
-                }]
-                },
-            3: {
-                'name': 'Ham and Pineapple',
-                'description': 'A delicious tomato based pizza topped with mozzarella, ham and pineapple',
-                'options': [{
-                    'size': 9,
-                    'price': 7.95
-                }, {
-                    'size': 12,
-                    'price': 12.95
-                }]
-                }
-        },
-        orders: [],
-        currentUser: null
-    },
-    getters: {
-        getMenuItems: state => state.menuItems,
-        numberOfOrders: state => state.orders.length,
-        currentUser: state => state.currentUser
-    },
-    mutations: {
-        addOrder: (state, orders) => state.orders.push(orders),
-        userStatus (state, user) {
-            if (user) {
-                state.currentUser = user.email
-            } else {
-                state.currentUser = null
-            }
-        }
-    },
-    actions: {
-        setUser(context, user) {
-            context.commit('userStatus', user)  
-        }               
+    modules: {
+        menu,
+        orders,
+        users
     }
 })
